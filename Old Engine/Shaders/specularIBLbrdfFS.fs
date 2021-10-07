4b645e9334f10e7c4cabe782201d9436ac773da6
MaanasTaneja <63515983+MaanasTaneja@users.noreply.github.com>
2021-10-07 20:50:52 +0530

    Update specularIBLbrdfFS.fs

--- Old Engine/Shaders/specularIBLbrdfFS.fs
+++ Old Engine/Shaders/specularIBLbrdfFS.fs
@@ -3,9 +3,7 @@ out vec2 FragColor;
 in vec2 TexCoords;
 
 const float PI = 3.14159265359;
-// ----------------------------------------------------------------------------
-// http://holger.dammertz.org/stuff/notes_HammersleyOnHemisphere.html
-// efficient VanDerCorpus calculation.
+
 float RadicalInverse_VdC(uint bits) 
 {
      bits = (bits << 16u) | (bits >> 16u);
@@ -15,12 +13,12 @@ float RadicalInverse_VdC(uint bits)
      bits = ((bits & 0x00FF00FFu) << 8u) | ((bits & 0xFF00FF00u) >> 8u);
      return float(bits) * 2.3283064365386963e-10; // / 0x100000000
 }
-// ----------------------------------------------------------------------------
+
 vec2 Hammersley(uint i, uint N)
 {
 	return vec2(float(i)/float(N), RadicalInverse_VdC(i));
 }
-// ----------------------------------------------------------------------------
+
 vec3 ImportanceSampleGGX(vec2 Xi, vec3 N, float roughness)
 {
 	float a = roughness*roughness;
@@ -43,7 +41,8 @@ vec3 ImportanceSampleGGX(vec2 Xi, vec3 N, float roughness)
 	vec3 sampleVec = tangent * H.x + bitangent * H.y + N * H.z;
 	return normalize(sampleVec);
 }
-// ----------------------------------------------------------------------------
+
+
 float GeometrySchlickGGX(float NdotV, float roughness)
 {
     // note that we use a different k for IBL
@@ -65,7 +64,7 @@ float GeometrySmith(vec3 N, vec3 V, vec3 L, float roughness)
 
     return ggx1 * ggx2;
 }
-// ----------------------------------------------------------------------------
+
 vec2 IntegrateBRDF(float NdotV, float roughness)
 {
     vec3 V;
@@ -105,7 +104,7 @@ vec2 IntegrateBRDF(float NdotV, float roughness)
     B /= float(SAMPLE_COUNT);
     return vec2(A, B);
 }
-// ----------------------------------------------------------------------------
+
 void main() 
 {
     vec2 integratedBRDF = IntegrateBRDF(TexCoords.x, TexCoords.y);
